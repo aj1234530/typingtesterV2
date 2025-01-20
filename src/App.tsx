220e5484e2b653e8c2f6f85ab4e87a17ef7cb66e
mainabhihun <youmailmejust123@gmail.com>
2025-01-20 23:28:34 +0530

    stats logic added

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     cursor.current? (cursor.current.style.transform = `translate(${cursorPosition.current.x}px,${cursorPosition.current.y}px)`): null;
   }, []);
 
-  const [paragraph] = useState(GenereateRandomParagraph()); //not destructing setPara as it is not neede
+  const [paragraph, setParagraph] = useState(GenereateRandomParagraph()); //not destructing setPara as it is not neede
   const words = useRef(paragraph.split(" "));
   const inputRef = useRef<HTMLInputElement>(null); //The generic <HTMLInputElement> defines the type of the value after the component mounts. //The generic <HTMLInputElement> defines the type of the value after the component mounts.
   const activeWord = useRef(0);
@@ -22,6 +22,8 @@ function App() {
   const cursor = useRef<HTMLDivElement | null>(null); //cursor refernce
 
   const stats = useRef({
+    typingStarted: false,
+    startTime: 0,
     noOfCorrectWords: 0,
     noOfIncorrectWords: 0,
     noOfWordsTyped: 0,
@@ -29,11 +31,21 @@ function App() {
     grossTypingSpeed: 0,
     accuracy: 0,
   });
-  console.log(wordsElementRef.current);
+  //stats ref to pass props
+  const accuracySpanRef = useRef<null | HTMLSpanElement>(null);
+  const netSpeedSpanRef = useRef<null | HTMLSpanElement>(null);
+  const grossSpeedSpanRef = useRef<null | HTMLSpanElement>(null);
+
   const letterElementRef = useRef<HTMLSpanElement | null>(null);
   //prettier-ignore
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(!stats.current.typingStarted){
+      stats.current.startTime = Date.now();
+      stats.current.typingStarted = true;
+    }
     console.log(stats.current);
+
+    if (e.key === "Backspace") { e.preventDefault(); }
     if (inputRef.current?.value.trim().length === 0 && e.key === " ") {
       e.preventDefault(); // Prevent space from being typed as the first character
     }
@@ -51,73 +63,86 @@ function App() {
         stats.current.noOfIncorrectWords++;
       }
       //game over logic
-      stats.current.accuracy =
-        (stats.current.noOfCorrectWords / words.current.length) * 100;
+      stats.current.accuracy =(stats.current.noOfCorrectWords / words.current.length) * 100;
+      stats.current.grossTypingSpeed = Math.ceil(stats.current.noOfWordsTyped / ((Date.now() - stats.current.startTime) / (1000 * 60))); 
+      grossSpeedSpanRef.current && (grossSpeedSpanRef.current.textContent = `${stats.current.grossTypingSpeed}`)
+
+      //why st - as was alert first and color thing later
       setTimeout(() => {
-        alert("hi");
+        alert(`Game Over: Accuracy: ${stats.current.accuracy}, Net Speed: ${stats.current.netTypingSpeed}, Gross Speed: ${stats.current.grossTypingSpeed}`);
       }, 0);
     } 
     //NOTE - impact of space when the space is pressed(handle correct, inc , checing if the length of input is not 0 i.e. the input is not empty after spaces )
     else if (inputRef.current && inputRef.current?.value.trim().length != 0 && e.key === " ") {
       if (inputRef.current.value.trim() === words.current[activeWord.current]) {
-        stats.current.noOfCorrectWords++;
+        stats.current.noOfCorrectWords++; 
         wordsElementRef.current[activeWord.current].style.color = "green";
       } else {
         wordsElementRef.current[activeWord.current].style.color = "red";
         stats.current.noOfIncorrectWords++;
       }
       stats.current.noOfWordsTyped++; //for accuracy in rt
-      stats.current.accuracy = (stats.current.noOfCorrectWords / stats.current.noOfCorrectWords) * 100;
+      stats.current.accuracy = (stats.current.noOfCorrectWords / stats.current.noOfWordsTyped ) * 100;
+      stats.current.netTypingSpeed = Math.ceil(
+        (stats.current.noOfCorrectWords - stats.current.noOfIncorrectWords) /
+        ((Date.now() - stats.current.startTime) / (1000 * 60))
+      );
+
       inputRef.current.value = "";
       activeWord.current++;
-
       //changing cursor position
       cursorPosition.current.y = wordsElementRef.current[activeWord.current].getBoundingClientRect().top;
       cursorPosition.current.x =wordsElementRef.current[activeWord.current].getBoundingClientRect().left;
       //updating cursor position
       cursor.current ? (cursor.current.style.transform = `translate(${cursorPosition.current.x}px,${cursorPosition.current.y}px)`) : null;
-    }
+      //prevent cursor's position change by handle businnes logic, if not preventvented the cursor won't go to the next word(will go and again will reset to prev position) 
+      e.preventDefault() 
+      accuracySpanRef.current && (accuracySpanRef.current.textContent = `${stats.current.accuracy}`);
+      netSpeedSpanRef.current && (netSpeedSpanRef.current.textContent = `${stats.current.netTypingSpeed }`);
+
+    } 
   };
 
   //prettier-ignore
   const handleTypingBusinessLogic = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     wordsElementRef.current[activeWord.current].style.fontWeight = "bold";
     //letter node increasing  
     if(inputRef.current?.value.trim().length === 1){
-letterNodes.current.currentNode = 0;
+      letterNodes.current.currentNode = 0;
       letterNodes.current.noOfNodes = 0;
     }
-    // if (letterNodes.current.currentNode < letterNodes.current.noOfNodes) {
-    //   letterNodes.current.currentNode++;
-    // }
     if (inputRef.current && inputRef.current?.value.trim().length === 1) {
-
       //TODO - revise this onefiletr the node and save(from gpt)
       childNodes.current  = Array.from(wordsElementRef.current[activeWord.current].childNodes) .filter((node): node is HTMLElement => node instanceof HTMLElement);
-      // const childNodes = 
       letterNodes.current.noOfNodes = childNodes.current.length;
-      
-     
-      // if (
-      //   words.current[activeWord.current].length < inputRef.current.value.length
-      // ) {
-      // }
-    //   cursor.current
-    //     ? (cursor.current.style.transform = `translate(${cursorPosition.current.x}px,${cursorPosition.current.y}px)`)
-    //     : null;
-    // }
    }
    if (childNodes.current[letterNodes.current.currentNode] instanceof HTMLElement) {
     cursorPosition.current.x = (childNodes.current[letterNodes.current.currentNode] as HTMLElement).getBoundingClientRect().right;
     cursorPosition.current.y = (childNodes.current[letterNodes.current.currentNode] as HTMLElement).getBoundingClientRect().top;
-    console.log('child node', letterNodes.current)
+
     cursor.current?(cursor.current.style.transform = `translate(${cursorPosition.current.x}px,${cursorPosition.current.y}px)`): null;
     letterNodes.current.currentNode++
-    console.log("sdfjsldfjsldfjl;asdf",childNodes.current.length,letterNodes.current)
   }
   };
+  const handleParaChange = () => {
+    window.location.reload();
+    // setParagraph(GenereateRandomParagraph());
 
+    inputRef.current && (inputRef.current.value = "");
+    (cursorPosition.current.x = 0),
+      (cursorPosition.current.y = 0),
+      (stats.current = {
+        typingStarted: false,
+        startTime: 0,
+        noOfCorrectWords: 0,
+        noOfIncorrectWords: 0,
+        noOfWordsTyped: 0,
+        netTypingSpeed: 0,
+        grossTypingSpeed: 0,
+        accuracy: 0,
+      });
+    activeWord.current = 0;
+  };
   return (
     <div>
       <div className="h-screen w-screen bg-red-500 flex flex-col">
@@ -127,11 +152,16 @@ letterNodes.current.currentNode = 0;
         <main className="h-[calc(100%-3rem)] bg-blue-100 w-full ">
           <button
             className="border rounded bg-blue-700 text-white p-2 "
-            onClick={() => alert("hi")}
+            onClick={handleParaChange}
           >
             Change Paragraph
           </button>
-          <TypingStats stats={stats} />
+          <TypingStats
+            stats={stats}
+            accuracySpanRef={accuracySpanRef}
+            grossSpeedSpanRef={grossSpeedSpanRef}
+            netSpeedSpanRef={netSpeedSpanRef}
+          />
           <div className="flex flex-col justify-center h-1/2 w-full ">
             <div
               className=" typing-para max-w-[600px] p-2  flex flex-row gap-1 "
@@ -195,14 +225,29 @@ letterNodes.current.currentNode = 0;
 
 export default App;
 
-function TypingStats({ stats }: { stats: any }) {
-  console.log(stats);
-  const [netSpeed] = useState(stats.current.accuracy);
+function TypingStats({
+  stats,
+  accuracySpanRef,
+  netSpeedSpanRef,
+  grossSpeedSpanRef,
+}: {
+  stats: any;
+  accuracySpanRef: React.MutableRefObject<null | HTMLSpanElement>;
+  netSpeedSpanRef: React.MutableRefObject<null | HTMLSpanElement>;
+  grossSpeedSpanRef: React.MutableRefObject<null | HTMLSpanElement>;
+}) {
   return (
     <div className="typing-stats-section  h-12 w-full border rounded h">
-      Stats :<span>Accuracy: </span>
-      <span>Net Speed:{netSpeed}</span>
-      <span>Gross Speed:</span>
+      Stats :
+      <span>
+        Accuracy:<span className="font-bold" ref={accuracySpanRef}></span>{" "}
+      </span>
+      <span>
+        Net Speed:<span className="font-bold" ref={netSpeedSpanRef}></span>
+      </span>
+      <span>
+        Gross Speed:<span className="font-bold" ref={grossSpeedSpanRef}></span>
+      </span>
     </div>
   );
 }
