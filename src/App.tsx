1ccf86cd82a532792a4ad901c55b2a2e07d40b0f
mainabhihun <youmailmejust123@gmail.com>
2025-01-25 12:37:11 +0530

    added a page for testing route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootPage from "./pages/RootPage";
 import DashboardPage from "./pages/DashboardPage";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootPage />}></Route>
         <Route path="/dashboard" element={<DashboardPage />}></Route>
-        <Route path="/*" element={<DashboardPage />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
